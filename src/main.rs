5de6f177d4f30fa1c7448de6a24e48ba93dfd62a
togami2864 <tuabobo123@gmail.com>
2022-10-30 11:42:17 +0900

    feat: health_check

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::TcpListener;
+
+use falilvfan::run;
+
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
+    let port = listener.local_addr().unwrap().port();
+    run(listener)?.await
 }
