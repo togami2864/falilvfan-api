26adeacc61b6a79db72620734a45086a8242cb51
togami2864 <tuabobo123@gmail.com>
2022-11-03 13:24:17 +0900

    chore: refactor

--- src/routes/register_lives.rs
+++ src/routes/register_lives.rs
@@ -5,20 +5,30 @@ use sqlx::PgPool;
 use uuid::Uuid;
 
 #[derive(Debug, serde::Deserialize)]
+pub struct SetListData {
+    track_id: String,
+    track_order: i32,
+}
+
+#[derive(Debug, serde::Deserialize)]
 pub struct LivesData {
     location_id: String,
     date: String,
     is_festival: bool,
+    setlist_data: Vec<SetListData>,
 }
 
 #[post("/register/lives")]
 async fn register_lives(req: web::Json<LivesData>, pool: web::Data<PgPool>) -> HttpResponse {
+    let live_id = Uuid::new_v4();
     let location_id = Uuid::from_str(&req.location_id).unwrap();
     let date = sqlx::types::chrono::NaiveDate::parse_from_str(&req.date, "%Y/%m/%d").unwrap();
-    match sqlx::query!(
-        r#"INSERT INTO lives (location_id, date, is_festival)
-    VALUES ($1, $2, $3)
+
+    if let Err(e) = sqlx::query!(
+        r#"INSERT INTO lives (live_id, location_id, date, is_festival)
+    VALUES ($1, $2, $3, $4)
     "#,
+        live_id,
         location_id,
         date,
         req.is_festival
@@ -26,10 +36,29 @@ async fn register_lives(req: web::Json<LivesData>, pool: web::Data<PgPool>) -> H
     .execute(pool.get_ref())
     .await
     {
-        Ok(_) => HttpResponse::Ok().finish(),
-        Err(e) => {
-            println!("Failed to execute query: {}", e);
-            HttpResponse::InternalServerError().finish()
-        }
+        println!("Failed to execute query: {}", e);
+        return HttpResponse::InternalServerError().finish();
+    };
+
+    for track_data in req.setlist_data.iter() {
+        let setlist_id = Uuid::new_v4();
+        let track_id = Uuid::from_str(&track_data.track_id).unwrap();
+        match sqlx::query!(
+            r#"INSERT INTO setlists (id, live_id, track_id, track_order) VALUES($1, $2, $3, $4)"#,
+            setlist_id,
+            live_id,
+            track_id,
+            track_data.track_order
+        )
+        .execute(pool.get_ref())
+        .await
+        {
+            Err(e) => {
+                println!("Failed to execute query: {}", e);
+                return HttpResponse::InternalServerError().finish();
+            }
+            _ => continue,
+        };
     }
+    HttpResponse::Ok().finish()
 }
---
+++ tests/register_new_items.rs
@@ -0,0 +1,232 @@
+#![allow(clippy::bool_assert_comparison)]
+use reqwest::header::CONTENT_TYPE;
+use sqlx::postgres::types::PgInterval;
+use test_helpers::TestApp;
+use uuid::Uuid;
+
+use crate::test_helpers::spawn_app;
+
+mod test_helpers;
+
+async fn insert_sample_album(app: &TestApp) -> Uuid {
+    let album_id = Uuid::new_v4();
+    let release_date =
+        sqlx::types::chrono::NaiveDate::parse_from_str("2022/10/26", "%Y/%m/%d").unwrap();
+    sqlx::query!(
+        r#"INSERT INTO albums (album_id, album_name, spotify_id, release_date, is_single)
+    VALUES ($1, $2, $3, $4, $5)
+    "#,
+        album_id,
+        "Cocoon for the Golden Future",
+        "05eS7MkETxSTk4UcyieA4s",
+        release_date,
+        false
+    )
+    .execute(&app.db_pool)
+    .await
+    .unwrap();
+    album_id
+}
+
+async fn insert_sample_locations(app: &TestApp) -> Uuid {
+    sqlx::query!(
+        r#"INSERT INTO prefectures (id, prefecture)
+    VALUES ($1, $2)
+    "#,
+        14,
+        "神奈川県"
+    )
+    .execute(&app.db_pool)
+    .await
+    .unwrap();
+
+    let location_id = Uuid::new_v4();
+    sqlx::query!(
+        r#"INSERT INTO locations (id, location, prefecture_id)
+    VALUES($1, $2, $3)
+        "#,
+        location_id,
+        "KT Zepp Yokohama",
+        14
+    )
+    .execute(&app.db_pool)
+    .await
+    .unwrap();
+
+    location_id
+}
+
+async fn insert_sample_tracks(app: &TestApp) -> Uuid {
+    let album_id = insert_sample_album(app).await;
+    let track_id = Uuid::new_v4();
+    let dur = std::time::Duration::from_millis(188000);
+    let dur = PgInterval::try_from(dur).unwrap();
+
+    sqlx::query!(
+        r#"INSERT INTO tracks (id, name, track_number, duration_ms, album_id, youtube_url)
+    VALUES ($1, $2, $3, $4, $5, $6)
+    "#,
+        track_id,
+        "Get Back the Hope",
+        1,
+        dur,
+        album_id,
+        "https://www.youtube.com/watch?v=aniw0eO_PlY&ab_channel=Fear%2CandLoathinginLasVegas"
+    )
+    .execute(&app.db_pool)
+    .await
+    .unwrap();
+
+    track_id
+}
+
+#[tokio::test]
+async fn return_200_for_register_new_album() {
+    let app = spawn_app().await;
+
+    let client = reqwest::Client::new();
+    let body = format!(
+        r#"{{
+"album_name": "{}",
+"spotify_id": "{}",
+"release_date": "{}",
+"is_single": false
+    }}"#,
+        "Cocoon for the Golden Future", "05eS7MkETxSTk4UcyieA4s", "2022/10/26"
+    );
+    let response = client
+        .post(format!("{}/register/album", &app.address))
+        .header(CONTENT_TYPE, "application/json")
+        .body(body)
+        .send()
+        .await
+        .expect("Failed to execute request.");
+
+    assert_eq!(200, response.status().as_u16());
+
+    let saved = sqlx::query!("SELECT album_name, spotify_id, is_single, release_date FROM albums")
+        .fetch_one(&app.db_pool)
+        .await
+        .expect("Failed to fetch saved albums");
+
+    assert_eq!(saved.album_name, "Cocoon for the Golden Future");
+    assert_eq!(saved.spotify_id.len(), 22);
+    assert_eq!(
+        saved.release_date,
+        sqlx::types::chrono::NaiveDate::parse_from_str("2022/10/26", "%Y/%m/%d").unwrap()
+    );
+
+    assert_eq!(saved.is_single, false);
+}
+
+#[tokio::test]
+async fn return_200_for_register_new_location() {
+    let app = spawn_app().await;
+
+    sqlx::query!(
+        r#"INSERT INTO prefectures (id, prefecture)
+    VALUES ($1, $2)
+    "#,
+        14,
+        "神奈川県"
+    )
+    .execute(&app.db_pool)
+    .await
+    .unwrap();
+
+    let client = reqwest::Client::new();
+    let body = format!(
+        r#"{{"location": "KT Zepp Yokohama", "prefecture_id": {}}}"#,
+        14
+    );
+
+    let response = client
+        .post(format!("{}/register/locations", &app.address))
+        .header(CONTENT_TYPE, "application/json")
+        .body(body)
+        .send()
+        .await
+        .expect("Failed to execute request.");
+
+    assert_eq!(200, response.status().as_u16());
+
+    let saved = sqlx::query!("SELECT location, prefecture_id FROM locations")
+        .fetch_one(&app.db_pool)
+        .await
+        .expect("Failed to fetch saved albums");
+
+    assert_eq!(saved.location, "KT Zepp Yokohama");
+    assert_eq!(saved.prefecture_id, 14);
+}
+
+#[tokio::test]
+async fn return_200_for_register_new_track() {
+    let app = spawn_app().await;
+
+    let client = reqwest::Client::new();
+    let album_id = insert_sample_album(&app).await;
+
+    let body = format!(
+        r#"{{
+"name": "{}",
+"track_number": {},
+"duration_ms": {},
+"album_id": "{}",
+"youtube_url": "{}"
+    }}"#,
+        "Get Back the Hope",
+        1,
+        188000,
+        album_id,
+        "https://www.youtube.com/watch?v=aniw0eO_PlY&ab_channel=Fear%2CandLoathinginLasVegas"
+    );
+
+    let response = client
+        .post(format!("{}/register/tracks", &app.address))
+        .header(CONTENT_TYPE, "application/json")
+        .body(body)
+        .send()
+        .await
+        .expect("Failed to execute request.");
+
+    assert_eq!(200, response.status().as_u16());
+
+    let saved = sqlx::query!("SELECT name, track_number, duration_ms, youtube_url FROM tracks")
+        .fetch_one(&app.db_pool)
+        .await
+        .expect("Failed to fetch saved albums");
+
+    assert_eq!(saved.name, "Get Back the Hope");
+    assert_eq!(saved.track_number, 1);
+    // modify millisecond
+    assert_eq!(saved.duration_ms.microseconds / 1000, 188000);
+    assert_eq!(
+        saved.youtube_url,
+        "https://www.youtube.com/watch?v=aniw0eO_PlY&ab_channel=Fear%2CandLoathinginLasVegas"
+    );
+}
+
+#[tokio::test]
+async fn return_200_for_register_new_live() {
+    let app = spawn_app().await;
+
+    let location_id = insert_sample_locations(&app).await;
+    let track_id = insert_sample_tracks(&app).await;
+
+    let body = format!(
+        r#"{{"location_id": "{}", "date": "2022/09/30", "is_festival": true, "setlist_data": [{{"track_id": "{}", "track_order": {}}}]
+    }}"#,
+        location_id, track_id, 1
+    );
+
+    let client = reqwest::Client::new();
+    let response = client
+        .post(format!("{}/register/lives", &app.address))
+        .header(CONTENT_TYPE, "application/json")
+        .body(body)
+        .send()
+        .await
+        .expect("Failed to execute request.");
+
+    assert_eq!(200, response.status().as_u16());
+}
